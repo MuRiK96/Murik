d8161c622de38591e77849384c2fccf441d5e9bc
MuRiK96 <murashkin_dima@mail.ru>
2024-05-13 20:48:27 +0300

    решение дз 6
    ответ

---
+++ poki/task1.py
@@ -0,0 +1,20 @@
+# from os import strerror
+# try:
+#     with open('C:/Users/7/Documents/GitHub/Murik/poki/content1.txt') as file:
+#         text = file.read()
+
+# except IOError as err:
+#     print(f'фаил не открылся{strerror(err.errno)}')
+# print(text)
+# word_f = input('введите слово,для изменения')
+# word_c = input('введите слово,для замены')
+
+# text = text.replace(word_f,word_c)
+# print(text)
+# try:
+#     with open('C:/Users/7/Documents/GitHub/Murik/poki/content1.txt','w') as file:
+#         file.write(text)
+# except IOError as err:
+#     print(f'фаил так и не открылся {strerror(err.errno)}')
+
+
